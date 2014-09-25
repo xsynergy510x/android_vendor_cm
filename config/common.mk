39d093581d46ecf1d1da27d598e24698ae3a2f68
Eduard Alfonso <xsynergy510x@gmail.com>
2014-09-24 18:35:03 -0700

    Remove CM apps I don't use
    Conflicts:
    	config/common.mk

--- config/common.mk
+++ config/common.mk
@@ -142,18 +142,9 @@ PRODUCT_PACKAGES += \
 
 # Custom CM packages
 PRODUCT_PACKAGES += \
-    Launcher3 \
-    Trebuchet \
-    DSPManager \
     libcyanogen-dsp \
     audio_effects.conf \
-    CMWallpapers \
-    Apollo \
-    CMFileManager \
-    LockClock \
-    CMUpdater \
-    CMAccount \
-    CMHome
+    CMAccount
 
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
