67a7b801a38d6e6285b5ea6075b12310d2ecc4d5
Paul Beeler <pbeeler80@gmail.com>
2015-05-21 16:42:24 -0600

    Include SaberMod vendor
    Change-Id: I0434d0777c294ee2536263e4d1c92b6e79a139d9
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Conflicts:
    	config/common.mk

--- config/common.mk
+++ config/common.mk
@@ -324,3 +324,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 -include vendor/cyngn/product.mk
 
 $(call prepend-product-if-exists, vendor/extra/product.mk)
+
+# Inherite sabermod vendor
+SM_VENDOR := vendor/sm
+include $(SM_VENDOR)/Main.mk
