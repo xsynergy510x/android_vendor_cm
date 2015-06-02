ae920070214ad141b1f5d5890b5e364bbd594c94
Eduard Alfonso <xsynergy510x@gmail.com>
2015-06-02 16:43:53 -0700

    Revert "common: disable secure ADB"
    This reverts commit e6a392cb9d1d21a389754e9aaf820f0d77837a26.

--- config/common.mk
+++ config/common.mk
@@ -68,7 +68,7 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 # Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
